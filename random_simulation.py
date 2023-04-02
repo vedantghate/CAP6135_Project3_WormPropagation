fcfac2f35cdcf7826ab6e7035d6c553b4898888b
vedantghate <wantedgutsy@gmail.com>
2023-04-02 14:59:10 -0400

    Initial commit

---
+++ random_simulation.py
@@ -0,0 +1,68 @@
+import random
+import matplotlib.pyplot as plt
+
+# Define the total number of IP addresses and the number of vulnerable computers
+TOTAL_IPS = 50000
+VULNERABLE_COMPUTERS = set(range(1, 11)).union(set(range(1001, 1011))).union(set(range(2001, 2011)))
+for i in range(4, 50):
+    VULNERABLE_COMPUTERS = VULNERABLE_COMPUTERS.union(set(range(i * 1000 + 1, i * 1000 + 11)))
+NON_VULNERABLE_COMPUTERS = set(range(1, TOTAL_IPS + 1)) - VULNERABLE_COMPUTERS
+
+# Define the initial infected computer
+INITIAL_INFECTED_COMPUTER = 2010
+
+# Define the scan rate of the worm
+SCAN_RATE = 4
+
+# Define the number of simulation runs
+NUM_RUNS = 3
+
+
+# Define the function that simulates the worm propagation
+def simulate_worm_propagation(scan_rate):
+    # Initialize the infected computers set and the time variable
+    infected_computers = {INITIAL_INFECTED_COMPUTER}
+    time = 0
+    time_step = []
+    num_infected = []
+    while len(infected_computers) < len(VULNERABLE_COMPUTERS):
+        # Generate the list of IP addresses to scan
+        ips_to_scan = []
+        for infected_computer in infected_computers:
+            for _ in range(scan_rate):
+                ip_to_scan = random.randint(1, TOTAL_IPS)
+                while ip_to_scan in infected_computers or ip_to_scan in ips_to_scan:
+                    ip_to_scan = random.randint(1, TOTAL_IPS)
+                ips_to_scan.append(ip_to_scan)
+        # Check if any of the scanned IP addresses are vulnerable
+        for ip_to_scan in ips_to_scan:
+            if ip_to_scan in VULNERABLE_COMPUTERS:
+                infected_computers.add(ip_to_scan)
+        # Update the time variable
+        time += 1
+        time_step.append(time)
+        num_infected.append(len(infected_computers))
+    return time_step, num_infected
+
+
+# Simulate the worm propagation for the specified number of runs
+simulation_results = []
+for i in range(NUM_RUNS):
+    print("Simulation ", (i + 1))
+    time, infected = simulate_worm_propagation(SCAN_RATE)
+    simulation_results.append((time, infected))
+    print(f"Finished in {time[-1]} time units\n")
+
+# Plot the simulation results
+plt.figure(figsize=(10, 6))
+plt.xlabel('Time')
+plt.ylabel('Number of Infected Computers')
+# plt.xlim(0, max([max(time) for time, infected in simulation_results]))
+# plt.ylim(0, len(VULNERABLE_COMPUTERS))
+plt.title('Worm Propagation Simulation')
+colors = ['red', 'green', 'blue']
+linestyles = ['solid', 'dashed', 'dotted']
+for i, (time, infected) in enumerate(simulation_results):
+    plt.plot(time, infected, label=f'Simulation {i+1}', color=colors[i], linestyle=linestyles[i])
+plt.legend()
+plt.show()
---
+++ sequential_simulation.py
@@ -0,0 +1,74 @@
+import random
+import matplotlib.pyplot as plt
+
+# Vulnerable computers' IP addresses
+vulnerable_ips = []
+initial_infected_ip = 2010
+
+for i in range(0, 500, 10):
+    for j in range(i + 1, i + 11):
+        vulnerable_ips.append(j)
+        if j == 2010:
+            initial_infected_ip = j
+
+
+# Define function to simulate worm propagation
+def simulate_worm_propagation(scan_rate, sequential_probability):
+    # Initialize variables
+    infected_ips = [initial_infected_ip]
+    sequential_ips = []
+    random_ips = []
+    for i in range(1, 500):
+        if random.random() < sequential_probability:
+            sequential_ips.append(vulnerable_ips[i])
+        else:
+            random_ips.append(vulnerable_ips[i])
+    time_steps_in = [0]
+    num_infected_in = [1]
+
+    # Simulate worm propagation until all vulnerable computers are infected
+    while len(infected_ips) < 500:
+        newly_infected_ips = []
+        for infected_ip in infected_ips:
+            if infected_ip in sequential_ips:
+                target_ip = (infected_ip % 50000) + 1
+                if target_ip in vulnerable_ips and target_ip not in infected_ips:
+                    newly_infected_ips.append(target_ip)
+            else:
+                for i in range(scan_rate):
+                    target_ip = random.randint(1, 50000)
+                    if target_ip in vulnerable_ips and target_ip not in infected_ips:
+                        newly_infected_ips.append(target_ip)
+        infected_ips += newly_infected_ips
+        time_steps_in.append(time_steps_in[-1] + 1)
+        num_infected_in.append(len(infected_ips))
+
+    return time_steps_in, num_infected_in
+
+
+# Run simulations and store results
+simulations = []
+for i in range(3):
+    sequential_prob = 0.7
+    if random.random() < 0.3:
+        sequential_prob = 0.3
+    time_steps, num_infected = simulate_worm_propagation(4, sequential_prob)
+    simulations.append((time_steps, num_infected))
+
+# Print time for all vulnerable computers to be infected in each simulation
+for i, sim in enumerate(simulations):
+    time_steps, num_infected = sim
+    finish_time = time_steps[-1]
+    print(f"Simulation {i + 1} finish time: {finish_time}")
+
+# Plot results
+fig, ax = plt.subplots()
+colors = ['red', 'green', 'blue']
+linestyles = ['solid', 'dashed', 'dotted']
+for i, sim in enumerate(simulations):
+    time_steps, num_infected = sim
+    ax.plot(time_steps, num_infected, label=f"Simulation {i + 1}", color=colors[i], linestyle=linestyles[i])
+ax.set_xlabel("Time")
+ax.set_ylabel("Number of Infected Computers")
+ax.legend()
+plt.show()
